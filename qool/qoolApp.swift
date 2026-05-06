0c3d24c1a973830169c129c8cc87da700f5f6741
Greatenishioka <20e1118@andrew.ac.jp>
2026-05-06 23:52:05 +0900

    Initial Commit

---
+++ qool/qoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  qoolApp.swift
+//  qool
+//
+//  Created by 西岡 on 2026/05/06.
+//
+
+import SwiftUI
+
+@main
+struct qoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ qoolTests/qoolTests.swift
@@ -0,0 +1,17 @@
+//
+//  qoolTests.swift
+//  qoolTests
+//
+//  Created by 西岡 on 2026/05/06.
+//
+
+import Testing
+@testable import qool
+
+struct qoolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
